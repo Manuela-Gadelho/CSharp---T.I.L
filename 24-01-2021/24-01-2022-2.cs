5e27fef41bb34dbf03797cc809eb030ffedf0915
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-01-24 15:54:29 -0300

    Create 24-01-2022-2.cs

---
+++ 24-01-2021/24-01-2022-2.cs
@@ -0,0 +1,16 @@
+Um array é uma estrutura de dados que contém uma série de dados ordenados, chamados "elementos". Os elementos são referenciados por número ordinal (índice), primeiro elemento é 1, segundo 2, etc. Os elementos podem ser de qualquer tipo, string, caractere, numérico, data, etc.
+Obtendo o índice de um item do Array: int ind = ...
+Inverter a ordem dos itens no Array: Array.Rev... 
+
+
+S.O.L.I.D 
+
+SOLID é um acrônimo dos cinco primeiros princípios da programação orientada a objetos e design de código identificados por Robert C. ... O acrônimo SOLID foi introduzido por Michael Feathers, após observar que os cinco princípios poderiam se encaixar nesta palavra.
+
+Os padrões SOLID são considerados uma boa prática de programação orientada a objetos que visam diminuir o acoplamento entre classes e separar responsabilidades como forma de melhorar o código da aplicação desenvolvida. SOLID é um acrônimo para cada um dos cinco princípios que fazem parte desse grupo: Single Responsability Principle (Princípio da Responsabilidade Única); Open/Closed Principle (Princípio do “Aberto para Extensão/Fechado para Implementação); Liskov Substitution Principle (Princípio da Substituição de Liskov);
+
+
+Clean Code, ou Código Limpo, é uma filosofia de desenvolvimento de softwares que consiste […] ... Clean Code, ou Código Limpo, é uma filosofia de desenvolvimento de softwares que consiste em aplicar técnicas simples que facilitam a escrita e a leitura de um código. Tornando-o, assim, de fácil compreensão.
+
+
+
