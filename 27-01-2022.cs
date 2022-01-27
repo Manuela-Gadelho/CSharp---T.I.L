456360a82acdfc42d20687788e3b2bb7f241ee47
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-01-27 16:15:11 -0300

    Create 27-01-2022.cs

---
+++ 27-01-2022.cs
@@ -0,0 +1,2 @@
+Polimorfismo significa muitas formas , na orientação a objetos você pode enviar uma mesma mensagem para diferentes objetos e fazê-los responder da maneira correta. Você pode enviar a mensagem mover para cada objeto semelhante a um veiculo e cada um vai se comportar de maneira diferente para atender a sua solicitação.
+
