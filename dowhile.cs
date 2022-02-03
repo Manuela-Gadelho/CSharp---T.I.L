aafcbafbbc41c0e709833b1d769d93d7514e520a
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-03 10:12:24 -0300

    Create dowhile.cs

---
+++ dowhile.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+            int contador2 = 0;
+
+            do
+            {
+                Console.WriteLine("do while!");
+            } while (contador2 < 20);
+            Console.ReadLine();
+        }
+        
+        }
+
+    }
+
