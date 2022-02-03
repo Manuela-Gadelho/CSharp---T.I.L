889899e9c9fbad38336ee1fec0b3699c6255802d
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-03 13:53:45 -0300

    Create POO.cs

---
+++ POO.cs
@@ -0,0 +1,12 @@
+encapsulamento 
+
+O encapsulamento é o processo de ocultar ou esconder os membros de uma classe do acesso exterior usando modificadores de acesso. ... Um modificador de acesso define o escopo e a visibilidade de um membro da classe. A linguagem C# suporta os seguintes modificadores de acesso: Public.
+
+Os Benefícios do Encapsulamento:
+Modularidade: o código-fonte para um objeto pode ser escrito e mantido independentemente do código-fonte de outros objetos (desacoplamento). ...
+Ocultação de informações: um objeto possui uma interface pública que outros objetos podem utilizar para comunicarem-se com ele.
+
+Abstração. A abstração é o ponto de partida para a criação de programas utilizando POO. Trata-se da capacidade de extrair dos personagens ou dos itens presentes no contexto, suas principais características, criando, dessa forma, objetos.
+
+Polimorfismo significa muitas formas , na orientação a objetos você pode enviar uma mesma mensagem para diferentes objetos e fazê-los responder da maneira correta. Você pode enviar a mensagem mover para cada objeto semelhante a um veiculo e cada um vai se comportar de maneira diferente para atender a sua solicitação.
+
