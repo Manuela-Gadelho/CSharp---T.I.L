df66f135ac0bc6c3eddf804cc51bf62ac2f52d7d
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-03 11:27:09 -0300

    Create lenght.cs

---
+++ lenght.cs
@@ -0,0 +1,4 @@
+Lenght é uma variável utilizada dentro de arrays, para medir de maneira inteligente o comprimento (quantidade de elementos) no array. 
+
+Length Property. The Length property returns the number of Char objects in this instance, not the number of Unicode characters because a Unicode character might be represented by more than one Char. Syntax: public int Length { get; } Return Value: It returns the number of Char objects in this instance of String.23 de jan. de 2019
+
