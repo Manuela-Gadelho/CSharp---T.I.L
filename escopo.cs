5230c4e52ac7b996a015e1fefaf09a0983fbdecf
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-03 11:56:00 -0300

    Create escopo.cs

---
+++ escopo.cs
@@ -0,0 +1,5 @@
+Em Ciência da Computação escopo é um contexto delimitante aos quais valores e expressões estão associados. Linguagens de programação têm diversos tipos de escopos. O tipo de escopo vai determinar quais tipos de entidades este pode conter e como estas são afetadas, em outras palavras, a sua semântica.
+
+Quando você declara um elemento de programa, como uma classe, função ou variável, seu nome só pode ser "visto" e usado em determinadas partes do programa. O contexto no qual um nome é visível é chamado de seu escopo.
+
+Existe um tipo de escopo chamado escopo global, quando criada uma variável dentro deste escopo ela é acessada por todas as funções do programa. 
