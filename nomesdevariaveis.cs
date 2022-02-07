02927b10600675a5ea8c3a321bad0e0598c973f4
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-07 14:21:05 -0300

    Create nomesdevariaveis.cs

---
+++ nomesdevariaveis.cs
@@ -0,0 +1,8 @@
+Os nomes de variáveis podem conter caracteres alfanuméricos e o caractere de sublinhado. Caracteres especiais como o símbolo de hash # (também conhecido como símbolo de número ou símbolo da libra) ou o símbolo do dólar $ não são permitidos.
+Os nomes de variáveis precisam começar com uma letra alfabética ou um sublinhado, não um número. Os desenvolvedores usam o sublinhado para uma finalidade especial, portanto, tente não usá-lo por enquanto.
+Nomes de variáveis não podem ser uma palavra-chave do C#. Por exemplo, você não pode usar as seguintes declarações de variável: decimal decimal; ou string string;.
+Os nomes de variáveis diferenciam maiúsculas de minúsculas, o que significa que string Value; e string value; são duas variáveis diferentes.
+Os nomes de variáveis devem usar minúsculas concatenadas, que é um estilo de escrita que usa uma letra minúscula no início da primeira palavra e uma letra maiúscula no início de cada palavra subsequente. Por exemplo, string thisIsCamelCase;.
+Nomes de variáveis devem ser descritivos e significativos no contexto do aplicativo. Escolha um nome para a variável que represente o tipo de dados que ela manterá.
+Os nomes de variáveis devem ser uma ou mais palavras inteiras unidas. Não use contrações, pois o nome da variável (e, portanto, sua finalidade) pode não ser claro para outras pessoas que estão lendo seu código.
+Nomes de variáveis não devem incluir o tipo de dados da variável. Você poderá ser aconselhado a usar um estilo como string strValue;. Esse conselho não é mais atual.
