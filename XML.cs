d2a82d50ff4a970580d4601be2a703b6a460fd02
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-09 15:57:06 -0300

    Create XML.cs

---
+++ XML.cs
@@ -0,0 +1,4 @@
+extensible markup language - xml
+linguagem de marcação, como o html. recomendada pela w3c. define regras para codificar diferentes documentos. É muito utilizado para a criação de Notas Fiscais Eletrônicas, também chamadas de NF-e, por armazená-las e ainda garantir uma assinatura digital.
+
+xml e sql server ? 
