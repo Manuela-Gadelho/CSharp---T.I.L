f928c91076c4bba01e19652de1857dec97ec5e3f
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-16 14:41:40 -0300

    Create 16-02-2022_construtores.cs

---
+++ 16-02-2022_construtores.cs
@@ -0,0 +1,3 @@
+Os construtores são tipos especiais de métodos usados ​​para criar e inicializar objetos. É através deste tipo especial de método, chamado de construtor, que você cria instâncias de uma classe.
+
+Assim, os construtores permitem que o programador crie objetos da classe e defina valores padrão, limite a instanciação e grave códigos flexíveis e fáceis de ler.
