4336f609464cc263eab8025ccebe59126108d828
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-18 15:37:14 -0300

    Create 18-02-2022.cs

---
+++ 18-02-2022.cs
@@ -0,0 +1,4 @@
+indexadores - c#
+
+são semelhantes às propriedades. De muitas maneiras, os indexadores baseiam-se nos mesmos recursos de linguagem que as propriedades. Os indexadores habilitam as propriedades indexadas: propriedades referenciadas com o uso de um ou mais argumentos. Esses argumentos fornecem um índice em um conjunto de valores.
+
