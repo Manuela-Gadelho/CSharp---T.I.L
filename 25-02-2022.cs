afcb8c1b894de8eee3da0b715244a4094803173c
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-02-25 13:37:32 -0300

    Create 25-02-2022.cs

---
+++ 25-02-2022.cs
@@ -0,0 +1,19 @@
+INSTRUÇÕES DE ITERAÇÃO 
+
+As instruções a seguir executam repetidamente uma instrução ou um bloco de instruções:
+
+A for instrução: executa seu corpo enquanto uma expressão booliana especificada é avaliada como true .
+A foreach instrução: enumera os elementos de uma coleção e executa seu corpo para cada elemento da coleção.
+A do instrução: executa condicionalmente seu corpo uma ou mais vezes.
+A while instrução: executa condicionalmente seu corpo zero ou mais vezes.
+A qualquer momento dentro do corpo de uma instrução de iteração, você pode sair do loop usando a instrução break ou ir para a próxima iteração no loop usando a instrução continue.
+
+Foreach 
+
+As instruções a seguir executam repetidamente uma instrução ou um bloco de instruções:
+
+A for instrução: executa seu corpo enquanto uma expressão booliana especificada é avaliada como true .
+A foreach instrução: enumera os elementos de uma coleção e executa seu corpo para cada elemento da coleção.
+A do instrução: executa condicionalmente seu corpo uma ou mais vezes.
+A while instrução: executa condicionalmente seu corpo zero ou mais vezes.
+A qualquer momento dentro do corpo de uma instrução de iteração, você pode sair do loop usando a instrução break ou ir para a próxima iteração no loop usando a instrução continue.
