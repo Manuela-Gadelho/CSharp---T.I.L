233ed413a512b5decbe2e4cc1e66f2932963415e
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-04-06 15:16:12 -0300

    Create 06-04-2022.cs

---
+++ 06-04-2022.cs
@@ -0,0 +1,7 @@
+Classes abstratas 
+
+É um tipo de classe especial que não pode ser instanciada, apenas herdada. Sendo assim, uma classe abstrata não pode ter um objeto criado a partir de sua instanciação. Essas classes são muito importantes quando não queremos criar um objeto a partir de uma classe “geral”, apenas de suas “subclasses”.
+
+métodos abstratos 
+
+Métodos Abstratos – Os métodos abstratos estão presentes somente em classes abstratas, e são aqueles que não possuem implementação. A sintaxe deste tipo de método é a seguinte: abstract ;
