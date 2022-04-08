b3b70f3c0d88eafee8773b0a9008aee3df6df30f
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-04-08 14:14:22 -0300

    Create 08-04-2022.cs

---
+++ 08-04-2022.cs
@@ -0,0 +1,8 @@
+palavra reservada throw 
+
+Indica a ocorrência de uma exceção durante a execução do programa.
+
+ArgumentException Classe
+
+A exceção que é gerada quando um dos argumentos fornecidos para um método não é válido.
+
