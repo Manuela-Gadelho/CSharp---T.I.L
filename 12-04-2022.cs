d80aaa90f8ee006ed060d5b58b4a90a78941d234
Manu Moreira <91551542+Manuela-Gadelho@users.noreply.github.com>
2022-04-12 15:52:53 -0300

    Create 12-04-2022.cs

---
+++ 12-04-2022.cs
@@ -0,0 +1,5 @@
+async and await
+
+The async keyword turns a method into an async method, which allows you to use the await keyword in its body. When the await keyword is applied, it suspends the calling method and yields control back to its caller until the awaited task is complete. await can only be used inside an async method.
+  
+  
